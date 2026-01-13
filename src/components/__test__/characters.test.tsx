49f598d8c1a623a360a5851d078bc4da984ebf8e
Sebastiano Silipo <sebastiano.silipo98@gmail.com>
2026-01-13 17:53:28 +0100

    [#ADD] Charactes test

---
+++ src/components/__test__/characters.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Characters from "../characters";
+import { Character } from "~/utils/types";
+
+describe("Characters component", () => {
+    test("renders the component title", () => {
+        render(<Characters />)
+
+        const titleElement = screen.getByText(/characters/i)
+        expect(titleElement).toBeInTheDocument()
+    })
+})
